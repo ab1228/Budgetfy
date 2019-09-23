3c86df5b12f9a1a49ec3ab940b22a579cfdb1361
ab1228 <alexb8734@gmail.com>
2019-09-22 22:55:53 -0500

    updated app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// BUDGET CONTROLLER
+var budgetController = (function () {
+
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.percentage = -1;
+    }
+})
