13910c66107ea7c0860122e1f60427f70b9ddc07
ab1228 <alexb8734@gmail.com>
2019-09-24 23:47:17 -0500

    modified app.js

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ var budgetController = (function () {
         this.percentage = -1;
     }
 })
+
+Expense.prototype.calcPercentage = function (totalIncome) {
+    if (totalIncome > 0) {
+        this.percentage = Math.round((this.value / totalIncome) * 100);
+    } else {
+        this.percentage = -1;
+    }
+};
