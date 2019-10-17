33d4d0b653f83da7d93894135fb74b6dfb8e85a1
ab1228 <alexb8734@gmail.com>
2019-10-17 14:22:52 -0500

    update UI for budget and percentage

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ var budgetController = (function () {
             if (data.totals.inc > 0) { /// calculate the percentage of income that we spend
                 data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
                 /* expense divided by income then multiplied by 100 get percentage */
-}
+            }
             else {
                 data.percentage = -1;
             }
@@ -111,7 +111,11 @@ var UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: 'budget__expenses--value',
+        percentageLabel: 'budget__expenses--percentage'
     }
 
     return {
@@ -165,6 +169,17 @@ var UIController = (function () {
 
             fieldsArray[0].focus();
         },
+        displayBudget: function (obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '----';
+            }
+        },
 
         getDOMstrings: function () {
             return DOMstrings;
@@ -198,7 +213,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         // return the budget
         var budget = budgetController.getBudget();
         // 5. display budget on UI
-        console.log(budget);
+        UIController.displayBudget(budget);
 
     }
 
